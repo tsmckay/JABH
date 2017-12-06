f739b4ef8d34bd42d21f6e38e09c5fdcb7b5cdc2
tsmckay <trevormckayhb@gmail.com>
2017-12-05 16:06:24 -0800

    changed game class to main class

--- JABHGame/src/edu/tsmckay/jabhgame/Game.java
+++ JABHGame/src/edu/tsmckay/jabhgame/Game.java
@@ -1,11 +1,20 @@
 package edu.tsmckay.jabhgame;
 
-public class Game {
+import javax.swing.JFrame;
+
+public class Main extends JFrame{
 
 	
-	public static void main(String[] args) {
-		// main game method
-
+	public static void main(String[] args)
+	{
+		new Main();
 	}
-
+	
+	public Main()
+	{
+		setTitle("JABH by Trevor McKay");
+		setSize(1000,1000);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+	}
 }
