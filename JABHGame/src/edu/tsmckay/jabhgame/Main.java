6c135ea61561f3a9e83964948591f1bfbfdb6fae
Programming <tsmckay100@student.hbuhsd.edu>
2017-12-07 07:45:44 -0800

    finished last commit

--- JABHGame/src/edu/tsmckay/jabhgame/Main.java
+++ JABHGame/src/edu/tsmckay/jabhgame/Main.java
@@ -4,20 +4,23 @@
  * tsmckay100@student.hbuhsd.edu
  */
 
-
 package edu.tsmckay.jabhgame;
 import javax.swing.JFrame;
 import java.awt.Rectangle;
 
-public class Main extends JFrame{
+
+/////////////////////////////////////////////////////
+
+public class Main extends JFrame
+{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		JFrame window = new JFrame();
 		window.setTitle("JABH by Trevor McKay");
 		window.setSize(1000,1000);
 		window.setResizable(false);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
-
 	}
 }
