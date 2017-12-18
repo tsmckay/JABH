9bf9f39e09c776297a9b2004b938da1c82a92c30
Programming <Programming@E-230-27-D17005.AD.HBUHSD.EDU>
2017-12-18 09:17:43 -0800

    made game set at 1080

--- game/src/edu/tsmckay/game/Game.java
+++ game/src/edu/tsmckay/game/Game.java
@@ -1,8 +1,10 @@
 package edu.tsmckay.game;
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Toolkit;
 import java.awt.image.BufferStrategy;
 
 public class Game extends Canvas implements Runnable{
@@ -10,8 +12,9 @@ public class Game extends Canvas implements Runnable{
 	private static final long serialVersionUID = 8219088514191419383L;
 	
 	//window and game width/height
-	public static final int WIDTH = 640, 
-							HEIGHT = WIDTH/12*9;
+	public Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	public static final int WIDTH = 1920,
+							HEIGHT = 1080;
 	
 	//declares thread, handler, spawner, menu, and HUD
 	private Thread thread;
@@ -47,7 +50,7 @@ public class Game extends Canvas implements Runnable{
 		AudioPlayer.getMusic("music").loop();
 		
 		//creates game window
-		new Window(WIDTH, HEIGHT, "Release 1.0", this);
+		new Window(screenSize, "Release 1.0", this);
 		
 		//initializes spawner
 		spawner = new Spawn(handler, hud);
--- game/src/edu/tsmckay/game/Window.java
+++ game/src/edu/tsmckay/game/Window.java
@@ -2,18 +2,20 @@ package edu.tsmckay.game;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class Window extends Canvas //new class windows which inherits canvas
 {
 	private static final long serialVersionUID = 7083802404550265596L;
 
-	public Window(int w, int h, String title, Game game)
+	public Window(Dimension size, String title, Game game)
 	{
 		JFrame frame = new JFrame(title);	//creates new frame
-		frame.setPreferredSize(new Dimension(w, h));	//
-		frame.setMinimumSize(new Dimension(w, h));		//sets size
-		frame.setMaximumSize(new Dimension(w, h));		//
+		frame.setPreferredSize(size);	//
+		frame.setMinimumSize(size);		//sets size
+		frame.setMaximumSize(size);	//
+		frame.setUndecorated(true);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//closes program when window is closed
 		frame.setResizable(false);		//prevents resizing
